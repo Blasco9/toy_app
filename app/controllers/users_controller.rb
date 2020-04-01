1c4e665f1e90a016c7ef22346685b13578bce377
Julian Blasco <blascoblasco9@gmail.com>
2020-04-01 20:25:09 -0300

    Generate Micropost model and migrate database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,7 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+      @microposts = @user.microposts
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
